1ae9f665cceb05e1c39d6848bedb6c6ce0a155be
kimgabiana <gabianz@yahoo.com>
2020-06-17 09:52:28 +0800

    add CRUD test

---
+++ tickets/src/index.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('missing JWT_KEY');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('missing MONGO_URI');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!!');
+});
+
+start();
